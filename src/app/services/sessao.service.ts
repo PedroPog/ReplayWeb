c0565248cbfe02e5cbb4c9429e878b8d00aaba44
Pedro Vieira <80628417+PedroPog@users.noreply.github.com>
2025-03-21 14:43:22 -0300

    service cript

--- src/app/services/sessao.service.ts
+++ src/app/services/sessao.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessaoService {
 
-  constructor() { }
+
+  salvarSessao(usuario:Usuario){
+    //sessionStorage.setItem()
+  }
+
+  obter(key:string){
+
+  }
+
+  limpar(){
+    sessionStorage.clear();
+  }
+
+
 }
