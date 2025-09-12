c305c8cde1a65f5e5f880960f131a5acb0ead847
Pedro Vieira <80628417+PedroPog@users.noreply.github.com>
2025-09-12 17:19:59 -0300

    .

--- src/app/components/home/components/lista-item/lista-item.component.ts
+++ src/app/components/home/components/lista-item/lista-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lista-item',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './lista-item.component.scss'
 })
 export class ListaItemComponent {
+
+  @Input() imagemUrl: any;
+  @Input() title: any;
+  @Input() descricao: any;
 
 }
